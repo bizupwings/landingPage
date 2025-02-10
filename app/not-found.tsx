cc44d0c44346c2e9c56dbec2d842fcfc962b3fe7
Naresh1435 <naresh45.dev@gmail.com>
2025-02-10 21:48:24 +0530

    style:addTopMargin not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
     return (
-      <div className="p-8 text-center">
+      <div className="p-8 mt-30 text-center">
         <h1 className="text-5xl font-bold text-red-500">404</h1>
         <p>Page not found!</p>
       </div>
