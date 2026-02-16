402c7d6b01b7582e93dd2382198e1e33900ffaef
bizupwings <shanghai.bizupwings@gmail.com>
2026-02-16 14:02:09 +0800

    Update Products.jsx

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -9,12 +9,13 @@ export default function Products({ filteredProducts = [], selfRender = false }) 
     ? products.slice(0, 10)
     : filteredProducts;
 
-  return (
-   <section className="py-2 bg-red-500">
-      <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold text-center text-gray-800 mb-10">
-          Products Center
-        </h2>
+return (
+  <section className="py-6 bg-gray-50">
+    <div className="container mx-auto px-4">
+      <h2 className="text-3xl font-bold text-center text-gray-800 mb-4">
+        Products Center
+      </h2>
+
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {displayProducts.map((product, index) => (
